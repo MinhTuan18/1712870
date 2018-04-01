b8dd4a50fe24e99d5b198d263a7cc43c5c6cd200
Your Name <xlightrayx@gmail.com>
2018-04-01 23:30:13 +0700

    tao project

---
+++ 1712870/1712870/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <io.h>
+#include <wchar.h>
+#include <stdlib.h>
+struct SINHVIEN
+{
+	wchar_t MSSV[10];
+	wchar_t HoTen[30];
+	wchar_t Khoa[30];
+	short KhoaHoc;
+	wchar_t NgaySinh[10];
+	wchar_t HinhCaNhan[20];
+	wchar_t Mota[1000];
+	wchar_t SoThich[100];
+};
